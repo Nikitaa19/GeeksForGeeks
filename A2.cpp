e6883d905ded22942c9ea72c8a1a6994174b4b09
Nikitaa19 <agarwalnikita064@gmail.com>
2023-09-09 00:51:55 +0530

    thirteenth commit

--- A2.cpp
+++ A2.cpp
@@ -106,6 +106,7 @@
 //     return 0;
 // }
 
+
 //Vector STL in C++
 
 // void fun(int a[], int n){
@@ -170,11 +171,273 @@
 //     return 0;
 // }
 
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     //size batane ke liye
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         cout<<v[i]<<endl;
+//     }
+// }
+// int main(){
+//     //in vector don't tell size of vector
+//     //just keep inserting, I will manage the allocations
+//     vector<int>v; //vector is a type of array
+//     //insert
+//     v.push_back(1);
+//     v.push_back(2);
+//     v.push_back(3);
+//     print(v);
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     //size batane ke liye
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         cout<<v[i]<<endl;
+//     }
+// }
+// int main(){
+//     //in vector don't tell size of vector
+//     //just keep inserting, I will manage the allocations
+//     vector<int>v; //vector is a type of array
+//     while(1){
+//         int d;
+//         cin>>d;
+//         v.push_back(d);
+//         cout<<"Capacity: "<<v.capacity()<<" Size: "<<v.size()<<endl;
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     //size batane ke liye
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         cout<<v[i]<<" ";
+//     }
+//     cout<<endl;
+// }
+// int main(){
+//     //in vector don't tell size of vector
+//     //just keep inserting, I will manage the allocations
+//     vector<int>v; //vector is a type of array
+//     //insert
+//     v.push_back(1);
+//     v.push_back(2);
+//     v.push_back(3);
+//     print(v);
+//     //pop->delete....jo ki last se hota hai
+//     v.pop_back();
+//     print(v);
+//     v.pop_back();
+//     print(v);
+//     v.pop_back();
+//     print(v);
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     cout<<"Printing vector: "<<endl;
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         cout<<v[i]<<" ";
+//     }
+//     cout<<endl;
+// } 
+// int main(){
+//     //in vector don't tell size of vector
+//     //just keep inserting, I will manage the allocations
+//     vector<int>v; //vector is a type of array
+//     //insert
+//    int n;
+//    cin>>n;
+//    for(int i=0;i<n;i++){
+//     int d;
+//     cin>>d;
+//     v.push_back(d);
+//    }
+//    print(v);
+//    for(int i=0;i<10;i++){
+//     v.push_back(80);
+//    }
+//    print(v);
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     cout<<"Printing vector: "<<endl;
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         //v.at->vector at, at->i th index pe jo hai woh print kardo
+//         cout<<v.at(i);
+//     }
+//     cout<<endl;
+// } 
+// int main(){
+//     //in vector don't tell size of vector
+//     //just keep inserting, I will manage the allocations
+//     vector<int>v; //vector is a type of array
+//     //insert
+//    int n;
+//    cin>>n;
+//    for(int i=0;i<n;i++){
+//     int d;
+//     cin>>d;
+//     v.push_back(d);
+//    }
+//    print(v);
+//    for(int i=0;i<10;i++){
+//     v.push_back(80);
+//    }
+//    print(v);
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     cout<<"Printing vector: "<<endl;
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         //v.at->vector at, at->i th index pe jo hai woh print kardo
+//         cout<<v.at(i)<<" ";
+//     }
+//     cout<<endl;
+// } 
+// int main(){
+//     //in vector don't tell size of vector
+//     //just keep inserting, I will manage the allocations
+//     vector<int>v; //vector is a type of array
+//     //insert
+//    int n;
+//    cin>>n;
+//    for(int i=0;i<n;i++){
+//     int d;
+//     cin>>d;
+//     v.push_back(d);
+//    }
+//    print(v);
+//    //to clear the vector
+//    v.clear();
+//    v.push_back(50);
+//    print(v);
+//    v.pop_back();
+//    print(v);
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     cout<<"Printing vector: "<<endl;
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         //v.at->vector at, at->i th index pe jo hai woh print kardo
+//         cout<<v.at(i)<<" ";
+//     }
+//     cout<<endl;
+// } 
+// int main(){
+//     //initilization
+//     vector<int>arr; //default with no data, 0 size
+//     vector<int>arr2(5, -1); //init with n size with specific data
+//     // arr2.push_back(50);
+//     // print(arr2);
+
+//     // vector<int>arr3={1,2,3,4,5};
+//     // arr3.pop_back();
+//     // print(arr3);
+
+//     vector<int>arr4{1,2,3,4,5};
+//     // print(arr4);
+
+//     //how to copy vector
+//     vector<int>arr5={1,2,3,4,5};
+//     vector<int>arr6(arr5);
+//     print(arr6);
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<char>v){
+//     cout<<"Printing vector: "<<endl;
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         //v.at->vector at, at->i th index pe jo hai woh print kardo
+//         cout<<v.at(i)<<" ";
+//     }
+//     cout<<endl;
+// } 
+// int main(){
+//     vector<char>v;
+//     v.push_back('a');
+//     v.push_back('b');
+//     v.push_back('c');
+//     cout<<"Front element: "<<v[0]<<endl;
+//     cout<<"Front element: "<<v.front()<<endl;
+//     cout<<"End element: "<<v[v.size()-1]<<endl;
+//     cout<<"End element: "<<v.back()<<endl;
+//     return 0;
+// }
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void print(vector<int>v){
+//     cout<<"Printing vector: "<<endl;
+//     int size=v.size();
+//     for(int i=0;i<size;i++){
+//         //v.at->vector at, at->i th index pe jo hai woh print kardo
+//         cout<<v.at(i)<<" ";
+//     }
+//     cout<<endl;
+// } 
+// int main(){
+//     vector<int>v;
+//     v.push_back(10);
+//     v.push_back(20);
+//     v.push_back(30);
+//     v.push_back(40);
+//     print(v);
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
 using namespace std;
+void print2(vector<int>v){
+    cout<<"Printing vector method 2: "<<endl;
+    for(auto it:v){
+        cout<<it<<" ";
+    }
+    cout<<endl;
+} 
 int main(){
-    //in vector don't tell size of vector
-    //just keep inserting, I will manage the allocations
+    vector<int>v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    print2(v);
     return 0;
 }
--- A3.cpp
+++ A3.cpp
@@ -1,6 +1,5 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hi"<<endl;
     return 0;
 }
