c7cb719d1bd5c3e46590d285328b0da383122ac3
Nikitaa19 <agarwalnikita064@gmail.com>
2024-01-03 21:25:00 +0530

    nineteenth commit

--- A3.cpp
+++ A3.cpp
@@ -1,5 +1,53 @@
 #include<iostream>
 using namespace std;
 int main(){
+    //create a 2D-Array
+    // int arr[3][3];
+
+    //initilization
+    // int arr[3][4]={{1,2,3,4}, {6,7,8,9}, {45,65,76,66}};
+    // int brr[]={1,2,3,4};
+    // int crr[][4]={{1,2,3,4}, {6,7,8,9}, {45,65,76,66}};
+    // //sirf rows define krne se ya dono na define krne se nhi chlega error ayega, column ka size btana pdega
+    // int drr[3][]={{1,2,3,4}, {6,7,8,9}, {45,65,76,66}};
+    // int err[][]={{1,2,3,4}, {6,7,8,9}, {45,65,76,66}};
+
+    //print
+    // void printArray(int arr[][4], int row, int col){
+    //     for(int i=0;i<row;i++){
+    //         for(int j=0;j<col;j++){
+    //             cout<<arr[i][j]<<" ";
+    //         }
+    //         cout<<endl;
+    //     }
+    // }
+    // //column wise print
+    // void colWisePrint(int arr[][4], int row, int col){
+    //     for(int i=0;i<col;i++){
+    //         for(int j=0;j<row;j++){
+    //             cout<<arr[j][i]<<" ";
+    //         }
+    //         cout<<endl;
+    //     }
+    // }
+    // int main(){
+    // int arr[3][4]={{1,2,3,4}, {6,7,8,9}, {45,65,76,66}};
+    // int row=3;
+    // int col=4;
+    // printArray(arr, row, col);
+    // colWisePrint(arr, row, col);
+
+    //input
+    //row wise input, column wise ke liye arr[j][i] hojyega
+    // int arr[3][3];
+    // int row=3;
+    // int col=3;
+    // for(int i=0;i<row;i++){
+    //     for(int j=0;j<col;j++){
+    //         cout<<"Enter the input for row index: "<<i<<" column index: "<<j<<endl;
+    //         cin>>arr[i][j];
+    //     }
+    // }
+
     return 0;
 }
