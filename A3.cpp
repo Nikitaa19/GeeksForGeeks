2a9272a90896693e0e5df937ccefbd855b8b9a7d
Nikitaa19 <agarwalnikita064@gmail.com>
2024-01-04 21:29:14 +0530

    twenty one

--- A3.cpp
+++ A3.cpp
@@ -176,13 +176,58 @@
 // }
 // cout<<endl;
 
-//insert
+//vector insert
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     vector<int> arr;
+//     arr.push_back(4);
+//     arr.push_back(8);
+//     arr.push_back(12);
+//     arr.push_back(11);
+//     arr.push_back(40);
+//     cout<<arr.size()<<endl;
+//     cout<<arr.empty()<<endl;
+
+//2D Array
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// int main(){
+//     // vector<vector<int>> arr(5, vector<int>(10,0));
+//     vector<vector<int>> arr(5, vector<int>(10,-1));
+//     //row size->arr.size()
+//     for(int i=0;i<arr.size();i++){
+//         //i th row column ka size
+//         for(int j=0;j<arr[i].size();j++){
+//             cout<<arr[i][j]<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+//Jagged Array
 #include<iostream>
 #include<vector>
 using namespace std;
 int main(){
-    vector<int> arr;
-
+    vector<vector<int>> brr;
+    vector<int> vec1(10,0);
+    vector<int> vec2(5,1);
+    vector<int> vec3(7,0);
+    vector<int> vec4(8,1);
+    vector<int> vec5(20,0);
+    brr.push_back(vec1);
+    brr.push_back(vec2);
+    brr.push_back(vec3);
+    brr.push_back(vec4);
+    brr.push_back(vec5);
+        for(int i=0;i<brr.size();i++){
+        for(int j=0;j<brr[i].size();j++){
+            cout<<brr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
